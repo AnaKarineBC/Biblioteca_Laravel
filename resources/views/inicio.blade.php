aea5cc2cc71076bfa0a790d47da318305788ba3f
AnaKarineBC <anakarinecadoso@gmail.com>
2022-05-27 09:45:22 -0300

    First

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1>Produtos</h1>
+    <form>
+        <fieldset>
+            <ul style="list-style: none;">
+                <li>
+                    <button><a href="/cadastrar">Cadastrar</a></button>
+                </li>
+                <br>
+                <li>
+                    <button><a href="/editar-produto/1">Editar</a></button>
+                </li>
+                <br>
+                <li>
+                    <button><a href="/listar-produto/1">Listar</a></button>
+                </li> 
+                <br>
+                <li>   
+                    <button><a href="/excluir-produto/1">Excluir</a></button>
+                </li>
+            </ul>
+        </fieldset>
+    </form>
+</body>
+</html>
